a6589f07809634089051d3a316b1fb8eaafa7154
Soares <mattheussbarbosa@hotmail.com>
2023-10-11 21:33:50 -0300

    ensure RemoteAuthentication throws UnexpectedError on 500

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -75,4 +75,20 @@ void main() {
       expect(future, throwsA(DomainError.unexpected));
     },
   );
+  test(
+    "Should trow UnexpectedError if HttpClient returns 500",
+    () async {
+      when(
+        httpClient.request(
+          url: anyNamed("url"),
+          method: anyNamed("method"),
+          body: anyNamed("body"),
+        ),
+      ).thenThrow(HttpError.serverError);
+
+      final future = sut.auth(params);
+
+      expect(future, throwsA(DomainError.unexpected));
+    },
+  );
 }
