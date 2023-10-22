513a96d53ea2e2ed35ec9f1081d4f4d67f1de642
Soares <mattheussbarbosa@hotmail.com>
2023-10-21 21:32:35 -0300

    ensure HttpAdaptar throws NotFoundError on 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -121,6 +121,14 @@ void main() {
       expect(future, throwsA(HttpError.forbidden));
     });
 
+    test("Should return NotFound if post returns 404", () async {
+      mockResponse(statusCode: 404);
+
+      final future = sut.request(url: url, method: "post");
+
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test("Should return ServerError if post returns 500", () async {
       mockResponse(statusCode: 500);
 
