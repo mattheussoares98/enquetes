9e462b021dc197de5ab380155e17350fc59569fe
Soares <mattheussbarbosa@hotmail.com>
2023-12-03 12:00:59 -0300

    move loginHeader to login components

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -1,5 +1,5 @@
 export 'app.dart';
-export 'login_header.dart';
+export '../pages/login/components/login_header.dart';
 export "head_line1.dart";
 export "spinner_dialog.dart";
 export "error_message.dart";
