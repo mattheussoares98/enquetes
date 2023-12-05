6a44644d1cc83028d2901fb735827d05a91b5d12
Soares <mattheussbarbosa@hotmail.com>
2023-12-04 22:21:41 -0300

    ensure ValidationComposite returns the first error found

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -1,8 +1,10 @@
-import 'package:enquetes/presentation/protocols/protocols.dart';
-import 'package:enquetes/validation/protocols/field_validation.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:enquetes/presentation/protocols/protocols.dart';
+import 'package:enquetes/validation/protocols/protocols.dart';
 
 class ValidationComposite implements Validation {
   final List<FieldValidation> validations;
@@ -13,7 +15,16 @@ class ValidationComposite implements Validation {
     @required String field,
     @required String value,
   }) {
-    return null;
+    String error;
+    for (var validation in validations.where(
+      (element) => element.field == field,
+    )) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    return error;
   }
 }
 
@@ -36,9 +47,10 @@ main() {
     validation1 = FieldValidationSpy();
     when(validation1.field).thenReturn("any_field");
     mockValidation1(null);
+
     validation2 = FieldValidationSpy();
     when(validation2.field).thenReturn("any_field");
-    mockValidation2("");
+    mockValidation2(null);
     sut = ValidationComposite([validation1, validation2]);
   });
   test(
@@ -47,6 +59,17 @@ main() {
       final error = sut.validate(field: "any_field", value: "any_value");
 
       expect(error, null);
+    },
+  );
+  test(
+    "Should return the first error",
+    () {
+      mockValidation1("error1");
+      mockValidation2("error2");
+
+      final error = sut.validate(field: "any_field", value: "any_value");
+
+      expect(error, "error1");
     },
   );
 }
