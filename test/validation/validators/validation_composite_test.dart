596c22fa545bbbed85ab0b8d7300a6fb209daa6d
Soares <mattheussbarbosa@hotmail.com>
2023-12-04 22:25:39 -0300

    ensure ValidationComposite returns the first error of the correct field

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -36,27 +36,28 @@ main() {
   FieldValidationSpy validation2;
 
   void mockValidation1(String error) {
-    when(validation1.validate(any)).thenReturn(error);
+    when(validation1.validate(any)).thenReturn(error == "" ? null : error);
   }
 
   void mockValidation2(String error) {
-    when(validation2.validate(any)).thenReturn(error);
+    when(validation2.validate(any)).thenReturn(error == "" ? null : error);
   }
 
   setUp(() {
     validation1 = FieldValidationSpy();
-    when(validation1.field).thenReturn("any_field");
+    when(validation1.field).thenReturn("first_field");
     mockValidation1(null);
 
     validation2 = FieldValidationSpy();
-    when(validation2.field).thenReturn("any_field");
+    when(validation2.field).thenReturn("second_field");
     mockValidation2(null);
     sut = ValidationComposite([validation1, validation2]);
   });
   test(
     "Should return null if all validations return null or empty",
     () {
-      final error = sut.validate(field: "any_field", value: "any_value");
+      mockValidation2("");
+      final error = sut.validate(field: "second_field", value: "any_value");
 
       expect(error, null);
     },
@@ -67,9 +68,9 @@ main() {
       mockValidation1("error1");
       mockValidation2("error2");
 
-      final error = sut.validate(field: "any_field", value: "any_value");
+      final error = sut.validate(field: "second_field", value: "any_value");
 
-      expect(error, "error1");
+      expect(error, "error2");
     },
   );
 }
