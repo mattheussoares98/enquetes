fae827aae7510f3131c05339c91e49937d1e228c
Soares <mattheussbarbosa@hotmail.com>
2023-12-04 21:38:45 -0300

    move ValidationComposite test setup to a helper method

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -24,19 +24,26 @@ main() {
   FieldValidationSpy validation1;
   FieldValidationSpy validation2;
 
+  void mockValidation1(String error) {
+    when(validation1.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation2(String error) {
+    when(validation2.validate(any)).thenReturn(error);
+  }
+
   setUp(() {
+    validation1 = FieldValidationSpy();
+    when(validation1.field).thenReturn("any_field");
+    mockValidation1(null);
+    validation2 = FieldValidationSpy();
+    when(validation2.field).thenReturn("any_field");
+    mockValidation2("");
     sut = ValidationComposite([validation1, validation2]);
   });
   test(
     "Should return null if all validations return null or empty",
     () {
-      validation1 = FieldValidationSpy();
-      when(validation1.field).thenReturn("any_field");
-      when(validation1.validate(any)).thenReturn(null);
-      validation2 = FieldValidationSpy();
-      when(validation2.field).thenReturn("any_field");
-      when(validation2.validate(any)).thenReturn("");
-
       final error = sut.validate(field: "any_field", value: "any_value");
 
       expect(error, null);
