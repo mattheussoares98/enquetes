a4ff113d32455af54b8b4fadbf6a46e63dae6892
Soares <mattheussbarbosa@hotmail.com>
2024-01-30 10:28:58 -0300

    ensure LocalStorageAdapter throws if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,6 +1,8 @@
 import 'package:enquetes/data/cache/cache.dart';
+import 'package:enquetes/domain/helpers/domain_error.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -21,19 +23,33 @@ class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 main() {
   SaveStorageAdapter sut;
   FlutterSecureStorageSpy secureStorage;
+  String key;
+  String value;
 
   setUp(() {
     secureStorage = FlutterSecureStorageSpy();
     sut = SaveStorageAdapter(secureStorage: secureStorage);
+    key = faker.lorem.word();
+    value = faker.guid.guid();
   });
 
+  void mockSaveSecureError() {
+    when(secureStorage.write(key: anyNamed("key"), value: anyNamed("value")))
+        .thenThrow(DomainError.unexpected);
+  }
+
   test("Should call save secure with correct values", () async {
-    final key = faker.lorem.word();
-    final value = faker.guid.guid();
     await sut.saveSecure(key: key, value: value);
 
     verify(
       secureStorage.write(key: key, value: value),
     );
+  });
+
+  test("Should throw if save secure throws", () {
+    mockSaveSecureError();
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
