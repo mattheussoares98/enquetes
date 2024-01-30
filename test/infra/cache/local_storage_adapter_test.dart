905ac6c277129bdac20ba7d051c98f20bd3db601
Soares <mattheussbarbosa@hotmail.com>
2024-01-30 10:59:18 -0300

    move LocalStorageAdapter to its own file

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -1,34 +1,21 @@
-import 'package:enquetes/data/cache/cache.dart';
-import 'package:enquetes/domain/helpers/domain_error.dart';
 import 'package:faker/faker.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:enquetes/domain/helpers/domain_error.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:enquetes/infra/cache/cache.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mockito/mockito.dart';
-
-class SaveStorageAdapter implements SaveSecureCacheStorage {
-  final FlutterSecureStorage secureStorage;
-
-  SaveStorageAdapter({@required this.secureStorage});
-
-  Future<void> saveSecure(
-      {@required String key, @required String value}) async {
-    await secureStorage.write(key: key, value: value);
-  }
-}
 
 class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
 main() {
-  SaveStorageAdapter sut;
+  LocalStorageAdapter sut;
   FlutterSecureStorageSpy secureStorage;
   String key;
   String value;
 
   setUp(() {
     secureStorage = FlutterSecureStorageSpy();
-    sut = SaveStorageAdapter(secureStorage: secureStorage);
+    sut = LocalStorageAdapter(secureStorage: secureStorage);
     key = faker.lorem.word();
     value = faker.guid.guid();
   });
