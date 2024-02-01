e00a5e14c9c07c75d8b9aee5684733700db24467
Mattheus Soares <mattheussbarbosa@hotmail.com>
2024-01-31 22:47:20 -0300

    ensure LocalStorageAdapter returns correct value

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 
 import '../../data/cache/cache.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage {
+class LocalStorageAdapter
+    implements SaveSecureCacheStorage, FetchSecureCacheStorage {
   final FlutterSecureStorage secureStorage;
 
   LocalStorageAdapter({@required this.secureStorage});
@@ -12,5 +13,9 @@ class LocalStorageAdapter implements SaveSecureCacheStorage {
   Future<void> saveSecure(
       {@required String key, @required String value}) async {
     await secureStorage.write(key: key, value: value);
+  }
+
+  Future<String> fetchSecure(String key) async {
+    return await secureStorage.read(key: key);
   }
 }
--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -20,23 +20,59 @@ main() {
     value = faker.guid.guid();
   });
 
-  void mockSaveSecureError() {
-    when(secureStorage.write(key: anyNamed("key"), value: anyNamed("value")))
-        .thenThrow(DomainError.unexpected);
-  }
+  group("save secure", () {
+    void mockSaveSecureError() {
+      when(secureStorage.write(key: anyNamed("key"), value: anyNamed("value")))
+          .thenThrow(DomainError.unexpected);
+    }
 
-  test("Should call save secure with correct values", () async {
-    await sut.saveSecure(key: key, value: value);
+    test("Should call save secure with correct values", () async {
+      await sut.saveSecure(key: key, value: value);
 
-    verify(
-      secureStorage.write(key: key, value: value),
-    );
+      verify(
+        secureStorage.write(key: key, value: value),
+      );
+    });
+
+    test("Should throw if save secure throws", () {
+      mockSaveSecureError();
+      final future = sut.saveSecure(key: key, value: value);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
   });
+  group("fetchSecure", () {
+    PostExpectation mockFetchSecure() => when(secureStorage.read(key: key));
+    mockFetchSecureCall() {
+      mockFetchSecure().thenAnswer((_) async => value);
+    }
 
-  test("Should throw if save secure throws", () {
-    mockSaveSecureError();
-    final future = sut.saveSecure(key: key, value: value);
+    mockFetchSecureError() {
+      mockFetchSecure().thenThrow(DomainError.unexpected);
+    }
 
-    expect(future, throwsA(DomainError.unexpected));
+    setUp(() {
+      mockFetchSecureCall();
+    });
+
+    test("Should return fetchedValue", () {
+      sut.fetchSecure(key);
+
+      verify(secureStorage.read(key: key));
+    });
+
+    test("Should call fetchSecure with correct value", () async {
+      final fetchedValue = await sut.fetchSecure(key);
+
+      expect(fetchedValue, value);
+    });
+
+    test("Should throw if fetchSecure throws", () {
+      mockFetchSecureError();
+
+      final future = sut.fetchSecure(key);
+
+      expect(future, throwsA(DomainError.unexpected));
+    });
   });
 }
