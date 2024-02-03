afa8347523247da476edfbd4d134e54119b20cc3
Mattheus Soares <mattheussbarbosa@hotmail.com>
2024-02-03 20:57:06 -0300

    move splash code to its own file

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+
+import 'package:flutter/material.dart';
+
+import 'splash.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  SplashPage({@required this.presenter});
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Builder(builder: (context) {
+        presenter.navigateToStream.listen((page) {
+          if (page?.isNotEmpty == true) {
+            Get.offAllNamed(page);
+          }
+        });
+
+        return Center(child: CircularProgressIndicator());
+      }),
+    );
+  }
+}
