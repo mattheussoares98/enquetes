e0daecbeb3e21f6fcbd98ac9b7b169bd2fd36c7f
Mattheus Soares <mattheussbarbosa@hotmail.com>
2024-02-27 22:18:22 -0300

    add surveyEntity

---
+++ lib/domain/entities/survey_entity.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:meta/meta.dart';
+
+class SurveyEntity extends Equatable {
+  final String id;
+  final String question;
+  final DateTime dateTime;
+  final bool didAnswer;
+
+  List<Object> get props => [
+        id,
+        question,
+        dateTime,
+        didAnswer,
+      ];
+
+  SurveyEntity({
+    @required this.id,
+    @required this.question,
+    @required this.dateTime,
+    @required this.didAnswer,
+  });
+}
